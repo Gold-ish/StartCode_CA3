7c733a24b1f163f373bdf15a6d57ed3441853b1f
789Michael <allanalli@hotmail.com>
2020-04-16 15:02:29 +0200

    fix user

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -18,30 +18,31 @@ public class SetupTestUsers {
     // CHANGE the three passwords below, before you uncomment and execute the code below
     // Also, either delete this file, when users are created or rename and add to .gitignore
     // Whatever you do DO NOT COMMIT and PUSH with the real passwords
-
-    User user = new User("user", "test1");
     User admin = new User("admin", "test1");
-    User both = new User("user_admin", "test1");
+    User user1 = new User("user1", "test123");
+    User user2 = new User("user2", "test123");
+    User user3 = new User("user3", "test123");
 
-    if(admin.getUserPass().equals("test")||user.getUserPass().equals("test")||both.getUserPass().equals("test"))
+    if(admin.getUserPass().equals("test")||user1.getUserPass().equals("test")||user2.getUserPass().equals("test")||user3.getUserPass().equals("test"))
       throw new UnsupportedOperationException("You have not changed the passwords");
 
     em.getTransaction().begin();
     Role userRole = new Role("user");
     Role adminRole = new Role("admin");
-    user.addRole(userRole);
+    user1.addRole(userRole);
+    user2.addRole(userRole);
+    user3.addRole(userRole);
     admin.addRole(adminRole);
-    both.addRole(userRole);
-    both.addRole(adminRole);
     em.persist(userRole);
     em.persist(adminRole);
-    em.persist(user);
+    em.persist(user1);
+    em.persist(user2);
+    em.persist(user3);
     em.persist(admin);
-    em.persist(both);
     em.getTransaction().commit();
-    System.out.println("PW: " + user.getUserPass());
-    System.out.println("Testing user with OK password: " + user.verifyPassword("test"));
-    System.out.println("Testing user with wrong password: " + user.verifyPassword("test1"));
+    System.out.println("PW: " + user1.getUserPass());
+    System.out.println("Testing user with OK password: " + user1.verifyPassword("test"));
+    System.out.println("Testing user with wrong password: " + user1.verifyPassword("test123"));
     System.out.println("Created TEST Users");
    
   }
